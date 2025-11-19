6563772dd0826acdba98cbb65a6aeb46893783ce
Vafantule <pavelgav@gmail.com>
2025-11-19 12:05:21 +0500

    add urls for registration password

--- users/urls.py
+++ users/urls.py
@@ -3,13 +3,14 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .apps import UsersConfig
+from .views import RegistrationView
 
 app_name = UsersConfig.name
 
 router = DefaultRouter()
-# router.register(r"habits", HabitViewSet, basename="habit")
+router.register(r"users", RegistrationView, basename="user")
 
 urlpatterns = [
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),  # маршрут для получения access и refresh токенов  # пояснение: POST username+password -> получает пару токенов
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ] + router.urls

2abf45177c43f3657f855b8d812fdb4bc1d6a83f
Vafantule <pavelgav@gmail.com>
2025-11-19 12:02:34 +0500

    add views for registration password

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from typing import Any
 
-# Create your views here.
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+from users.serializers import RegistrationSerializer
+
+
+class RegistrationView(generics.CreateAPIView):
+    """
+    Контроллер регистрации нового пользователя.
+    """
+    serializer_class = RegistrationSerializer
+
+    def create(self, request: Any, *args: Any, **kwargs: Any) -> Response:
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        header = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=header)

e13def9fa5d21edb6d4411531fb9b03b9db9478f
Vafantule <pavelgav@gmail.com>
2025-11-19 11:44:48 +0500

    add serializer for registration password

---
+++ users/serializers.py
@@ -0,0 +1,35 @@
+from typing import Dict, Any
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для регистрации пользователя.
+    """
+    password = serializers.CharField(write_only=True)
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "password", "password_confirm")
+        read_only_fields = ("id",)
+
+    def validate(self, attrs: Dict[str, Any]) -> Dict[str, Any]:
+        password = attrs.get("password")
+        password_confirm = attrs.pop("password_confirm", None)
+        if password != password_confirm:
+            raise serializers.ValidationError({"password_confirm": "Пароли не совпадают."})
+        validate_password(password)
+        return attrs
+
+    def create(self, validated_data: Dict[str, Any]) -> User:
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
